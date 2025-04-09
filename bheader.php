1b3c5a93917229e05077cf5d9f189de36348a827
kumarisupriya302 <kumarisupriya30200@gmail.com>
2025-04-09 17:06:44 +0530

    add bootstrap end comment

--- bheader.php
+++ bheader.php
@@ -37,6 +37,7 @@ if (!isset($_SESSION['user'])) {
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap css end -->
 
     <!-- Tailwind CSS -->
     <script src="https://cdn.tailwindcss.com"></script>
